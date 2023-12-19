e3cfa452559a9bf6f8e5969563eb3639bfd4ba8d
Isaiah <IaguileraCFE@gmail.com>
2023-12-19 11:57:19 -0800

    Isaiah Mock Final Push

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const url = "http://localhost:3000/ramens/"
 
 fetch("http://localhost:3000/ramens/")
-  .then((res) => res.json())
-  .then((data) => renderRamen(data))
+    .then((res) => res.json())
+    .then((data) => renderRamen(data))
 //   .then((data) => console.log(data));
 
 
@@ -14,54 +14,73 @@ function renderRamen(ramenArray) {
     // console.log(ramenImage);
 
     ramenArray.forEach((ramenBowl) => {
-    //   console.log(ramenBowl.ramenImage); 
-      
-    
-    const ramenImgTag = document.createElement("img");
-      ramenImgTag.src = ramenBowl.image;
-      ramenImage.append(ramenImgTag);
+        //   console.log(ramenBowl.ramenImage); 
 
-      ramenImgTag.addEventListener('click', handleClick)
-      function handleClick(){
-        //console.log(ramenBowl)
-    
-      }
+
+        const ramenImgTag = document.createElement("img");
+        ramenImgTag.src = ramenBowl.image;
+        ramenImage.append(ramenImgTag);
+
+        ramenImgTag.addEventListener('click', (e) => handleClick(e))
+        function handleClick(e) {
+            console.log(ramenBowl)
+
+            const nameLocation = document.querySelector('.name')
+            const imgLocation = document.querySelector('.detail-image')
+            const restLocation = document.querySelector('.restaurant')
+            const ratingLocation = document.querySelector('#rating-display')
+            const commentLocation = document.querySelector('#comment-display')
+
+            nameLocation.textContent = ramenBowl.name
+            imgLocation.src = ramenBowl.image
+            restLocation.textContent = ramenBowl.restaurant
+            ratingLocation.textContent = ramenBowl.rating
+            commentLocation.textContent = ramenBowl.comment
+
+
+
+        }
 
 
 
     });
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    const ramenForm = document.querySelector('form');
-    ramenForm.addEventListener('submit', (e) => handleNewRamen(e));
-
-function handleNewRamen(e) {
-    e.preventDefault();
-    console.log(e.target["new comment"].value)
-
-    // let newRamen = {
-    //     name : e.target.name.value,
-    //     restaurant : e.target.restaurant.value,
-    //     image : e.target.image.value,
-    //     rating : e.target.rating.value
-    //     comment : e.target.comment.value
-    // }
-    
 }
 
 
+
+const ramenForm = document.querySelector('#new-ramen');
+ramenForm.addEventListener('submit', (e)=> handleAddNewRamen(e))
+function handleAddNewRamen(e){
+    e.preventDefault()
+
+    // console.log(e.target.name)
+    const newRamen = {
+        name : e.target.name.value,
+        restaurant : e.target.restaurant.value,
+        image : e.target.image.value,
+        rating : e.target.rating.value,
+        comment : e.target['new-comment'].value
+    }
+
+
 }
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
